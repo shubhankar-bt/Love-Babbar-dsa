dcd1ea81d79ebd09218b9a1e224a93ebcb9f69c1
Shubhankar Das <78974094+shubhankar-bt@users.noreply.github.com>
2022-07-15 12:43:17 +0530

    Add files via upload

---
+++ 18:05:22/1st.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int array[] = {1,2,3,4,5};
+int i = 0;
+
+
+
+cout<<*(array+i)<<endl;
+
+}
---
+++ 18:05:22/2nd.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+//1
+int *p1;
+cout<<p1<<endl;
+cout<<&p1<<endl;
+cout<<*p1<<endl;
+
+
+
+// char ch = 'B';
+
+// char *ctr = &ch;
+
+// cout<<ctr<<endl;
+
+    return 0;
+}
